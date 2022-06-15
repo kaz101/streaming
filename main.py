0f2e89728bba1a10f760d371a91eaae01fec1990
Kevin Millard <kaz101130@gmail.com>
2022-06-15 00:14:34 -0400

    next

--- main.py
+++ main.py
@@ -3,10 +3,23 @@ import time
 
 driver = webdriver.Chrome()
 
-driver.get('https://projectfreetv.one/tv/star-trek-39401')
+driver.get('https://www.2embed.ru/library/tv/253')
 time.sleep(5)
-iframe = driver.find_element_by_id("iframe-embed")
-link = iframe.get_attribute('src')
-driver.get(link)
-time.sleep(2)
-button = driver.find_element_by_id('play-now')
+episode_dropdown = driver.find_element_by_id('ss-episodes-1')
+episode_dropdown.click()
+time.sleep(5)
+episodes = driver.find_elements_by_partial_link_text('Episode')
+ep_number = 0
+link_numbers = []
+for i in episodes:
+    link_numbers.append(str(ep_number))
+    ep_number +=1
+print(link_numbers)
+for i in link_numbers:
+    ep =driver.find_element_by_partial_link_text("Episode "+i)
+    ep.click()
+    time.sleep(.5)
+    link = driver.find_element_by_id("direct-link")
+    print(link)
+    episode_dropdown.click()
+    time.sleep(.5)
