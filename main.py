c8351a215f5ab53bf9c361f9564d7a98f148ea44
Kevin Millard <kaz101130@gmail.com>
2022-06-16 00:35:56 -0400

    stuff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
 
 #driver = webdriver.Chrome("/home/kaz/github/streaming/chromedriver")
@@ -6,41 +7,45 @@ driver = webdriver.Chrome()
 driver.get('https://www.2embed.ru/library/tv/253')
 
 def get_episodes(season):
-    episode_dropdown = driver.find_element_by_id('ss-episodes-'+season)
+    episode_dropdown = driver.find_element(By.ID, f'ss-episodes-{season}')
     episode_dropdown.click()
-    episodes = driver.find_elements_by_partial_link_text('Episode')
+    episodes = driver.find_elements(By.PARTIAL_LINK_TEXT, 'Episode')
     ep_number = 0
     link_numbers = []
+    episode_names = []
     for i in episodes:
         link_numbers.append(str(ep_number))
         ep_number +=1
     episode_links = []
     for i in link_numbers:
         try:
-            ep =driver.find_element_by_partial_link_text("Episode "+i)
+            ep = driver.find_element(By.PARTIAL_LINK_TEXT, f"Episode {i}")
             ep.click()
-            link = driver.find_element_by_id("direct-link").get_attribute("value")
+            link = driver.find_element(By.ID, "direct-link").get_attribute("value")
             episode_links.append(link)
             episode_dropdown.click()
         except:
             continue
-    return episode_links
-season_dropdown = driver.find_element_by_class_name('edit-season')
+    return episode_links, episode_names
+
+
+season_dropdown = driver.find_element(By.CLASS_NAME, 'edit-season')
 season_dropdown.click()
-seasons = driver.find_elements_by_partial_link_text("Season")
+seasons = driver.find_elements(By.PARTIAL_LINK_TEXT, "Season")
 season_numbers = []
 season_num = 1
 for i in seasons:
     season_numbers.append(str(season_num))
     season_num +=1
-print(season_numbers)
 final_links = []
 temp_links = []
+episode_names = []
 for i in season_numbers:
-    season_select = driver.find_element_by_partial_link_text("Season "+i)
+    season_select = driver.find_element(By.PARTIAL_LINK_TEXT, f"Season {i}")
     season_select.click()
-    temp_links = get_episodes(i)
-    for j in temp_links:
-        final_links.append(j)
+    temp_links, temp_names = get_episodes(i)
+    final_links = final_links + temp_links
+    episode_names = episode_names + temp_names
     season_dropdown.click()
-print(final_links)
+for i in range(len(final_links)):
+    print(final_links[i], episode_names[i])
